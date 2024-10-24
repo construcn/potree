41fa48a14cb2a5a27309e0ccfdf2f852a2a63b13
sivalaxman007 <sivalaxman.mylapilli@constructn.ai>
2024-10-24 15:34:01 +0530

    commenting pitch

--- src/navigation/OrbitControls.js
+++ src/navigation/OrbitControls.js
@@ -283,13 +283,13 @@ export class OrbitControls extends EventDispatcher {
 
       yaw -= progression * this.yawDelta;
       pitch -= progression * this.pitchDelta;
-	if (this.isInterior) {
-      if (pitch < -1 * 0.3) {
-        pitch = -1 * 0.3;
-      } else if (pitch > 1.3) {
-        pitch = 1.3;
-      }
-    }	
+	// if (this.isInterior) {
+  //     if (pitch < -1 * 0.3) {
+  //       pitch = -1 * 0.3;
+  //     } else if (pitch > 1.3) {
+  //       pitch = 1.3;
+  //     }
+  //   }	
       view.yaw = yaw;
       view.pitch = pitch;
 
